8693ff6652c5514557d948c1aef30632c0cb28d5
Saiprasad23209 <74244264+Saiprasad23209@users.noreply.github.com>
2021-06-04 11:48:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,50 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-var plygonimg;
-var engine, world;
-var block1,block2,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16
+var Dog,dogstand,dogsit
+
 function preload(){
-    plygonimg=loadImage("polygon.png")
+dogstand=loadImage("images/dogimg.png")
+dogsit=loadImage("images/dogimg1.png")
+
 }
+
 function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-    
-    ground=new Ground(600,380,1200,20);
-    Stand1= new Ground(390,260,200,10)
-    //level two
-    block8=new box(330,235,30,40);
-    block9=new box(360,235,30,40);
-    block10=new box(390,235,30,40);
-    block11=new box(420,235,30,40);
-    block12=new box(450,235,30,40);
-    //level three
-    block13=new box(360,195,30,40);
-    block14=new box(390,195,30,40);
-    block15=new box(420,195,30,40);
-    //top
-    block16=new box(390,155,30,40);
-
-    //polygon holder with slings
-    plygon = Bodies.circle(50,200,20);
-    World.add(world,plygon);
-
-   slingShot=new Slingshot(this.plygon,{x:100,y:200});
+    createCanvas(500,500);
+    database=firebase.database();
+    //foodobj=new Food()
+   foodStock = database.ref("Food");
+   foodStock.on("value",readStock);
+   Dog=createSprite(250,200,100,100);
+  Dog.addImage()
 }
+
 function draw(){
-    ground.display()
-    background("blue")
-    Engine.update(engine)
-    Stand1.display()
-    //level two
-    block8.display()
-    block9.display()
-   block10.display()
-    block11.display()
-    block12.display()
-    //level three
-    block13.display()
-    block14.display()
-    block15.display()
-    //top
-    block16.display()
-    imageMode(CENTER)
-    image(plygonimg,plygon.position.x,plygon.position.y,40,40)
-    slingShot.display()
-
-}
- 
-function mouseDragged(){
-    Matter.Body.setPosition(this.plygon,{x:mouseX,y:mouseY})
+    background("white");
+    
+if(keyDown(UP_ARROW)){
+    writeStock(foodS);
+    Dog.addImage(dogstand)
+    }
+    drawSprites();
 }
 
-function mouseReleased(){
-    slingShot.fly()
-}
+function writeStock(x){
+    if(x<=0){
+        x=o;}
+  else{
+      x=x-1;
+        }
+        database.ref('/').update({
+            Food:x
+        })
+    }
+
+    function readStock(data){
+        foodS=data.val();
+    }
+
+    function writeStock(x){
+
+        database.ref('/').update({
+            Food:x
+        })
+
+    }
